e0d139342990800da306959f6a17dd8d42944de5
William Owen <william.owen.bsc@gmail.com>
2024-10-22 20:40:48 -0700

    Good progress: background images, input styles

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,65 @@
 import './styles.scss';
 import authMessages from './authFormWelcomes.json'
 
-console.log("Hello World!d", { authMessages });
 
-writeFriendlyMessage()
 
-const dividerEl = document.querySelector('#auth-container')
-document.querySelectorAll('.toggleAuthType').forEach((el) => {
-    el.addEventListener('click', () => {
-        dividerEl.getAttribute('auth-mode') === 'login' ? dividerEl.setAttribute('auth-mode', 'signup') : dividerEl.setAttribute('auth-mode', 'login')
-        writeFriendlyMessage();
+    writeFriendlyMessage()
+
+    const dividerEl = document.querySelector('#auth-container')
+    document.querySelectorAll('.toggleAuthType').forEach((el) => {
+        el.addEventListener('click', (e) => {
+            e.stopPropagation()
+            dividerEl.getAttribute('auth-mode') === 'login' ? dividerEl.setAttribute('auth-mode', 'signup') : dividerEl.setAttribute('auth-mode', 'login')
+            writeFriendlyMessage();
+        })
     })
-})
 
-function writeFriendlyMessage() {
-    const loginMessage = authMessages['login-messages'][Math.floor(Math.random() * authMessages['login-messages'].length)]
-    const signupMessage = authMessages['signup-messages'][Math.floor(Math.random() * authMessages['signup-messages'].length)]
-    document.querySelector('.login').innerHTML = `<h2>${loginMessage}</h2>`
-    document.querySelector('.signup').innerHTML = `<h2>${signupMessage}</h2>`
-}
+    function writeFriendlyMessage() {
+        const loginMessage = authMessages['login-messages'][Math.floor(Math.random() * authMessages['login-messages'].length)]
+        const signupMessage = authMessages['signup-messages'][Math.floor(Math.random() * authMessages['signup-messages'].length)]
+        document.querySelectorAll('.login h2, .signup h2').forEach((el) => el.remove())
+        document.querySelector('.login').insertAdjacentHTML('afterbegin', `<h2>${loginMessage}</h2>`)
+        document.querySelector('.signup').insertAdjacentHTML('afterbegin', `<h2>${signupMessage}</h2>`)
 
-// Set listeners on all inputs for the label to float
-document.querySelectorAll('input').forEach((el) => {
-    el.addEventListener('focus', (e) => {
-        const target = e.target as HTMLInputElement;
-        target.closest('label').classList.add('moveLabel')
-     })
-    el.addEventListener('blur', (e) => {
-        const target = e.target as HTMLInputElement;
-        if(target.value === '') {
-            target.closest('label').classList.remove('moveLabel')
-        } 
-     })
-    el.addEventListener('change', (e) => { 
-        const target = e.target as HTMLInputElement;
-        if(target.value === '') {
-            target.closest('label').classList.remove('moveLabel')
-        } else {
+    }
+
+
+    // Set listeners on all inputs for the label to float
+    document.querySelectorAll('input').forEach((el) => {
+        el.addEventListener('focus', (e) => {
+            const target = e.target as HTMLInputElement;
             target.closest('label').classList.add('moveLabel')
-        }
+        })
+        el.addEventListener('blur', (e) => {
+            const target = e.target as HTMLInputElement;
+            if (target.value === '') {
+                target.closest('label').classList.remove('moveLabel')
+            }
+        })
+        el.addEventListener('change', (e) => {
+            const target = e.target as HTMLInputElement;
+            if (target.value === '') {
+                target.closest('label').classList.remove('moveLabel')
+            } else {
+                target.closest('label').classList.add('moveLabel')
+            }
+        })
     })
-})
 
-// Set listeners on toggle password visibility
-document.querySelectorAll('.password-toggle').forEach((el) => {
-    el.addEventListener('click', (e) => {
-        console.log('clicked')
-        const target = e.target as HTMLElement;
-        const label = target.closest('label')
-        const input = label.querySelector('input') as HTMLInputElement;
-        const isShowingPassword = label.getAttribute('password-toggle') == "hide" ? true : false
-        if(isShowingPassword) {
-            input.type = 'text';
-            label.setAttribute('password-toggle', "show")
-        } else {
-            input.type = 'password';
-            label.setAttribute('password-toggle', "hide")
-        }
-        
-      
+    // Set listeners on toggle password visibility
+    document.querySelectorAll('.password-toggle').forEach((el) => {
+        el.addEventListener('click', (e) => {
+            console.log('clicked')
+            const target = e.target as HTMLElement;
+            const label = target.closest('label')
+            const input = label.querySelector('input') as HTMLInputElement;
+            const isShowingPassword = label.getAttribute('password-toggle') == "hide" ? true : false
+            if (isShowingPassword) {
+                input.type = 'text';
+                label.setAttribute('password-toggle', "show")
+            } else {
+                input.type = 'password';
+                label.setAttribute('password-toggle', "hide")
+            }
+        })
     })
-})
