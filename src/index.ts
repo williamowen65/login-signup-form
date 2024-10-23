1421d9338b5eb29b6881186613fd2634e6804c87
William Owen <william.owen.bsc@gmail.com>
2024-10-22 18:39:57 -0700

    Friendly messages on covers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import './styles.scss';
+import authMessages from  './authFormWelcomes.json'
 
-console.log("Hello World!d");
+console.log("Hello World!d", {authMessages});
+
+writeFriendlyMessage()
+
 const dividerEl = document.querySelector('#auth-container')
-
 document.querySelectorAll('.toggleAuthType').forEach((el) => {
     el.addEventListener('click', () => {
         dividerEl.getAttribute('auth-mode') === 'login' ? dividerEl.setAttribute('auth-mode', 'signup') : dividerEl.setAttribute('auth-mode', 'login')
+        writeFriendlyMessage();
     })    
 })
+
+function writeFriendlyMessage(){
+    const loginMessage = authMessages['login-messages'][Math.floor(Math.random() * authMessages['login-messages'].length)] 
+    const signupMessage = authMessages['signup-messages'][Math.floor(Math.random() * authMessages['signup-messages'].length)] 
+    document.querySelector('.login').innerHTML = `<h2>${loginMessage}</h2>`
+    document.querySelector('.signup').innerHTML = `<h2>${signupMessage}</h2>`
+}
+
+
