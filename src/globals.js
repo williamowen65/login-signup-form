8f063e7b85f7ac70dfac94c49f2634d481fb0c69
William Owen <william.owen.bsc@gmail.com>
2024-10-26 07:10:21 -0700

    prod mode ?

--- src/globals.js
+++ src/globals.js
@@ -3,4 +3,4 @@ export const mode = {
     // DEVELOPMENT: 'development'
 }
 // export const root = mode.PRODUCTION ? "/login-sign-up-form": "/dist"
-export const root = "/login-sign-up-form"
+export const root = "/login-signup-form"
