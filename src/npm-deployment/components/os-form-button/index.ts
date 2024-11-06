882b8f119d6e7674f35a635a6ade622c837efaac
William Owen <william.owen.bsc@gmail.com>
2024-11-06 12:45:11 -0800

    Button submit type update

--- src/npm-deployment/components/os-form-button/index.ts
+++ src/npm-deployment/components/os-form-button/index.ts
@@ -5,28 +5,31 @@ import cssContent from "./style.scss";
 
 
 //@ts-ignore
-function getValue(shadowDom){
+function getValue(shadowDom) {
     return shadowDom.value()
 }
 
-createCustomElement('os-form-button',function() {
-        const type = this.getAttribute("type") || "button"; // Alt: provide submit 
+createCustomElement('os-form-button', function () {
+    const type = this.getAttribute("type") || "button"; // Alt: provide submit 
 
-       this.shadowRoot.querySelector('div').innerHTML = 
-       `<button type="${type}">
+    this.shadowRoot.querySelector('div').innerHTML =
+        `<button type="${type}">
             <slot><div style="">Unnamed Button</div><slot>
         </button>
         <div id="button-error" class="error-message"></div>
         `;
 
-        this.shadowRoot.querySelector('button').addEventListener('click', () => {
+    this.shadowRoot.querySelector('button').addEventListener('click', (e: Event) => {
+        e.preventDefault();
+        const target = e.target as HTMLButtonElement;
+        if (target.getAttribute("type") === "submit") {
             // Dispatch a custom submit event from os-form-button
             console.log("Get the form data for the form event")
             // You have to manually do this because of the custom component architecture of shadowDOM/shadowScope.
-            
             this.dispatchEvent(new CustomEvent('formSubmit', { bubbles: true, composed: true }));
-        });
+        }
+
+    });
 
 }, "<div></div>", cssContent);
 
-  
